ce22df5b553ceaec5adf2b7650e263e5423aa114
lavender010101 <lavender010101@163.com>
2021-10-20 21:16:51 +0800

    LeetCode-1567:Maximum Length of Subarray With Positive Product

---
+++ Collections/Dynamic_Programming/MaximumLengthOfSubarrayWithPositiveProduct.java
@@ -0,0 +1,60 @@
+/*
+Description
+	Given an array of integers nums, find the maximum length of a subarray where the product of all its elements is positive.
+	A subarray of an array is a consecutive sequence of zero or more values taken out of that array.
+	Return the maximum length of a subarray with positive product.
+
+Example-1
+
+```
+Input: nums = [1,-2,-3,4]
+Output: 4
+Explanation: The array nums already has a positive product of 24.
+```
+
+Example 2:
+
+```
+Input: nums = [0,1,-2,-3,-4]
+Output: 3
+Explanation: The longest subarray with positive product is [1,-2,-3] which has a product of 6.
+Notice that we cannot include 0 in the subarray since that'll make the product 0 which is not positive.
+```
+
+Example 3:
+
+```
+Input: nums = [-1,-2,-3,0,1]
+Output: 2
+Explanation: The longest subarray with positive product is [-1,-2] or [-2,-3].
+```
+
+Tags
+Greedy, Array, Dynamic Programming
+
+Solution
+Dynamic Programming
+ */
+class Solution {
+	public int getMaxLen(int[] nums) {
+		int positive = nums[0] > 0 ? 1 : 0; // record current length, tempory
+		int negtive = nums[0] < 0 ? 1 : 0;
+		int ans = positive;
+
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				positive++;
+				negtive = negtive > 0 ? negtive + 1 : 0;
+			} else if (nums[i] < 0) {
+				int tmp = negtive > 0 ? negtive + 1 : 0;
+				negtive = positive + 1;
+				positive = tmp;
+			} else {
+				positive = negtive = 0;
+			}
+			ans = Math.max(ans, positive);
+		}
+
+		return ans;
+	}
+}
